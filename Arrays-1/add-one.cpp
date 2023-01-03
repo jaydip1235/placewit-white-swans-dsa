dfdcb1aa7e4b984850cdbaf684b8304085e43c17
jaydip1235 <jaydipdeyalok@gmail.com>
2023-01-03 22:06:50 +0530

    commit

---
+++ Arrays-1/add-one.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+
+        int c = 1;
+        vector<int> ans;
+
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            int sum = digits[i] + c;
+            int num = sum % 10;
+            c = sum / 10;
+            ans.insert(ans.begin(), num);
+        }
+        if (c > 0)
+            ans.insert(ans.begin(), c);
+        return ans;
+    }
+};
