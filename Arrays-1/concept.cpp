13469e7f27992bafd79ea35c10fbf97962cb9b9f
jaydip1235 <jaydipdeyalok@gmail.com>
2023-01-03 21:49:34 +0530

    commit

---
+++ Arrays-1/concept.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+
+    // int arr[10];
+    // int *brr = new int[10];
+    // cout<<sizeof(brr)<<endl;
+    // delete[] brr;
+
+    int ** td = new int*[10];
+    for(int i=0;i<10;i++){
+        td[i]=new int[10];
+    }
+
+    return 0;
+}
---
+++ Arrays-1/max-consecustive-ones.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/max-consecutive-ones/
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+
+        int maxi = INT_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+                count++;
+            else
+                count = 0;
+            maxi = max(maxi, count);
+        }
+        return maxi;
+    }
+};
