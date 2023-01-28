a7e993917ed95f30922f98d4fbfaabac273c2f4d
jaydip1235 <jaydipdeyalok@gmail.com>
2023-01-28 15:42:18 +0530

    28jan

--- Strings/uncommonCharacters.cpp
+++ Strings/uncommonCharacters.cpp
@@ -1,7 +1,39 @@
-for (char c : st1)
+class Solution
 {
-    if (st2.find(c) == st2.end())
+public:
+    string UncommonChars(string a, string b)
     {
-        ans += c
+        set<char> st1;
+        set<char> st2;
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            st1.insert(a[i]);
+        }
+        for (int i = 0; i < b.size(); i++)
+        {
+            st2.insert(b[i]);
+        }
+
+        string ans = "";
+
+        for (char c : st1)
+        {
+            if (st2.find(c) == st2.end())
+            {
+                ans += c;
+            }
+        }
+        for (char c : st2)
+        {
+            if (st1.find(c) == st1.end())
+            {
+                ans += c;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        if (ans == "")
+            return "-1";
+        return ans;
     }
-}
+};
