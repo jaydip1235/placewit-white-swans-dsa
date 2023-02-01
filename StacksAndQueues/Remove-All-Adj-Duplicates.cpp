a4676f33d96df128bddf6327235cf3d543a0084e
jaydip1235 <jaydipdeyalok@gmail.com>
2023-02-01 21:19:35 +0530

    1st feb

---
+++ StacksAndQueues/Remove-All-Adj-Duplicates.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    string removeDuplicates(string s)
+    {
+
+        stack<char> st;
+        string ans;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (st.size() == 0)
+            {
+                st.push(s[i]);
+            }
+            else if (s[i] == st.top())
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
