c1493d573159d0d00884e59b3ce77376bf076183
jaydip1235 <jaydipdeyalok@gmail.com>
2023-02-04 15:01:52 +0530

    4th feb

---
+++ Math/SurfaceAreaOf3DShape.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int surfaceArea(vector<vector<int>> grid)
+    {
+        int res = 0, n = grid.size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+
+                if (grid[i][j])
+                {
+                    res += grid[i][j] * 4 + 2;
+                }
+                if (i >= 1)
+                    res -= min(grid[i][j], grid[i - 1][j]) * 2;
+                if (j >= 1)
+                    res -= min(grid[i][j], grid[i][j - 1]) * 2;
+            }
+        }
+        return res;
+    }
+};
